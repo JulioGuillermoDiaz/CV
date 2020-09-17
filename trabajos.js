cc67b14ee724150c303972f7eaa4749e7a38c6fb
Julio Guillermo Díaz <52174619+JulioGuillermoDiaz@users.noreply.github.com>
2020-09-17 10:38:51 -0300

    Add files via upload

---
+++ trabajos.js
@@ -0,0 +1,10 @@
+var trabajos = [
+    {    Anio : "2019 - 2020",
+      Empresa  : "Integra Software",
+    Posicion : "Tech Leader"},
+    {    Anio : "2017 - 2018",
+      Empresa  : "ISBAN",
+    Posicion : "Tech Leader"},
+    {    Anio : "2016 - 2013",
+      Empresa  : "Totvs",
+    Posicion : "Progress Architect"}];
