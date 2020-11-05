0205edf8143b8532c3eba17cab88699e54ada795
Julio Guillermo Díaz <52174619+JulioGuillermoDiaz@users.noreply.github.com>
2020-11-05 10:00:50 -0300

    jueves

--- cache/llenarfechas.js
+++ cache/llenarfechas.js
@@ -4,6 +4,7 @@ function llenarfechas() {
     new Date("11/2/2020"),
     new Date("11/3/2020"),
     new Date("11/4/2020"),
+    new Date("11/18/2020"),
     new Date("11/5/2020")
     ]
     return fechas;
